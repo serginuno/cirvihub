e57144c7de8b0388e7f6a4708719381644e2e5b7
Sergi Nuño <serginunodot@gmail.com>
2017-09-27 16:28:41 +0200

    Pujem l'arxiu suma Pero probar els errors garrafals que en toeria hauríen de succeir

---
+++ suma.java
@@ -0,0 +1,9 @@
+
+int numero1 = 0;
+int numero2 = 0;
+
+System.out.println("Introduce el primer numero:");
+numero1 = reader.nextInt();
+
+System.out.println("Introduce el segundo numero");
+numero2 = reader.nextInt();
